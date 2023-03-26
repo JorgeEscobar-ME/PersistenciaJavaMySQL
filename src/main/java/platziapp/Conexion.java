033e68b73a8d55b41b34a60026323f085ca42d4c
Jorge Escobar <jorgealejandroescobar0@gmail.com>
2023-03-26 18:21:27 -0500

    Primer commit persistencia de datos

---
+++ src/main/java/platziapp/Conexion.java
@@ -0,0 +1,20 @@
+package platziapp;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    public Connection get_connection(){
+        Connection connection = null;
+        try {
+            connection= DriverManager.getConnection("jdbc:mysql://localhost:/mensajes_app");
+            if (connection!=null){
+                System.out.println("Conexion exitosa");
+            }
+        }catch (SQLException e){
+            System.out.println(e);
+        }
+        return connection;
+    }
+}
